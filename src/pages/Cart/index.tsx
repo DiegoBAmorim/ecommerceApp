a4527ce1e3f308688baa38295f17430e60c8d501
Diego Barbosa <diegobarbosamorim@gmail.com>
2021-10-05 09:34:11 -0300

    Page Cart

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native'
+
+const Cart:React.FC = () => {
+    return(
+    <SafeAreaView>
+        <Text>Carrinho</Text>
+    </SafeAreaView>
+    )
+}
+
+export default Cart;
