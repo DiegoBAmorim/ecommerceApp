5c154d7df38f2b2cb91beff16c4e62d77b58ad82
Diego Barbosa <diegobarbosamorim@gmail.com>
2021-10-07 11:33:29 -0300

    update cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native'
+import {SafeAreaView, Text} from 'react-native';
+
+import {Container} from './styles'
 
 const Cart:React.FC = () => {
     return(
-    <SafeAreaView>
+    <Container>
         <Text>Carrinho</Text>
-    </SafeAreaView>
+    </Container>
     )
 }
 
