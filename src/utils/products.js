82a439fc095007029806cbffae48699ae356b525
Diego Amorim <66035875+DiegoBAmorim@users.noreply.github.com>
2021-11-23 00:29:29 -0300

    Update Products
    Dados mockados agora em js

---
+++ src/utils/products.js
@@ -0,0 +1,73 @@
+export default[
+  
+  {
+    "id": 201,
+    "name": "Call Of Duty Infinite Warfare",
+    "price": 49.99,
+    "score": 80,
+    "image": require("../assets/call-of-duty-infinite-warfare.png"),
+    "description": "Call of Duty: Infinite Warfare é um jogo eletrônico de tiro em primeira pessoa desenvolvido pela Infinity Ward e distribuído pela Activision. É o décimo terceiro jogo da franquia Call of Duty e foi lançado em 4 de novembro de 2016 para as plataformas Microsoft Windows, PlayStation 4 e Xbox One"
+  },
+  {
+    "id": 102,
+    "name": "The Witcher III Wild Hunt",
+    "price": 119.5,
+    "score": 250,
+    "image": require('../assets/the-witcher-iii-wild-hunt.png'),
+    "description": "The Witcher 3: Wild Hunt é um jogo eletrônico de RPG de ação em mundo aberto desenvolvido pela CD Projekt RED e lançado no dia 19 de maio de 2015 para as plataformas Microsoft Windows, PlayStation 4, Xbox One e em outubro de 2019 para o Nintendo Switch, sendo o terceiro título da série de jogos The Witcher."
+  },
+  {
+    "id": 99,
+    "name": "Call Of Duty WWII",
+    "price": 249.99,
+    "score": 205,
+    "image": require('../assets/call-of-duty-wwii.png'),
+    "description": "Call of Duty: WWII é um jogo eletrônico de tiro em primeira pessoa produzido pela empresa Sledgehammer Games e distribuído pela Activision para PlayStation 4, Xbox One e Microsoft Windows. É o primeiro jogo da série Call of Duty a se passar durante a Segunda Guerra Mundial desde Call of Duty: World at War, em 2008. "
+  },
+  {
+    "id": 12,
+    "name": "Mortal Kombat XL",
+    "price": 69.99,
+    "score": 150,
+    "image": require('../assets/mortal-kombat-xl.png'),
+    "description": "Mortal Kombat XL é a versão completa do Mortal Kombat X original, com todos os personagens, roupas, golpes, combos e outros adicionais que foram lançados por download pago ou gratuito. O jogo chega para Xbox One e PS4 em março de 2016 e, como vantagem, ele traz preço mais em conta."
+  },
+  {
+    "id": 74,
+    "name": "Shards of Darkness",
+    "price": 71.94,
+    "score": 400,
+    "image": require('../assets/shards-of-darkness.png'),
+    "description": "Styx: Shards of Darkness é um videogame furtivo desenvolvido pelo desenvolvedor francês Cyanide e publicado pela Focus Home Interactive. O jogo foi lançado mundialmente em 14 de março de 2017 para Microsoft Windows, PlayStation 4 e Xbox One."
+  },
+  {
+    "id": 31,
+    "name": "Terra Média: Sombras de Mordor",
+    "price": 79.99,
+    "score": 50,
+    "image": require('../assets/terra-media-sombras-de-mordor.png'),
+    "description": "Call of Duty: Infinite Warfare é um jogo eletrônico de tiro em primeira pessoa desenvolvido pela Infinity Ward e distribuído pela Activision. É o décimo terceiro jogo da franquia Call of Duty e foi lançado em 4 de novembro de 2016 para as plataformas Microsoft Windows, PlayStation 4 e Xbox One"
+  },
+  {
+    "id": 420,
+    "name": "FIFA 18",
+    "price": 195.39,
+    "score": 325,
+    "image": require('../assets/fifa-18.png'),
+    "description": "Call of Duty: Infinite Warfare é um jogo eletrônico de tiro em primeira pessoa desenvolvido pela Infinity Ward e distribuído pela Activision. É o décimo terceiro jogo da franquia Call of Duty e foi lançado em 4 de novembro de 2016 para as plataformas Microsoft Windows, PlayStation 4 e Xbox One"
+  },
+  {
+    "id": 312,
+    "name": "Super Mario Odyssey",
+    "price": 197.88,
+    "score": 100,
+    "image": require('../assets/super-mario-odyssey.png')
+  },
+  {
+    "id": 501,
+    "name": "Horizon Zero Dawn",
+    "price": 115.8,
+    "score": 290,
+    "image": require('../assets/horizon-zero-dawn.png')
+  }
+]
