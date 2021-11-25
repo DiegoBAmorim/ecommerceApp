251c104cae07d4f3575af0ddb161e8a909f1c8d3
Diego Amorim <66035875+DiegoBAmorim@users.noreply.github.com>
2021-11-24 23:33:29 -0300

    update cart layout and useSelector

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import { useSelector } from 'react-redux';
+import CartItems from '../../components/CartItems';
+import { allProductCart } from '../../store/CartProducts/CartProducts.selector';
 
-import {Container} from './styles'
+import {ContainerEmpty, Container, Title, List} from './styles'
 
 const Cart:React.FC = () => {
+    const cartProducts = useSelector(allProductCart)
+    if(cartProducts.length === 0){
+        return(
+        <ContainerEmpty>
+            <Title>Seu Carrinho está vazio :( </Title>
+        </ContainerEmpty> 
+        )
+    }
     return(
     <Container>
-        <Text>Carrinho</Text>
+        <Title>Carrinho</Title>
+        <List 
+        data={cartProducts}
+        keyExtractor={(item: any) => String(item.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => <CartItems data={item}/>}
+        />
     </Container>
     )
 }
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -1,5 +1,21 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const ContainerEmpty = styled.SafeAreaView`
 flex: 1;
+justify-content: center;
+align-items: center;
+`;
+
+export const Container = styled.SafeAreaView`
+flex: 1;
+`;
+
+export const Title = styled.Text`
+text-align: center;
+`;
+
+export const List = styled.FlatList`
+flex: 1;
+margin-top: 40px;
+border: 1px solid red;
 `;
