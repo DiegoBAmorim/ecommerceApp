d79ab4ffe5599a90c4681e48110d5418a5c9fb16
Diego Amorim <66035875+DiegoBAmorim@users.noreply.github.com>
2021-11-24 23:34:32 -0300

    update routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useSelector } from 'react-redux';
 
 import Cart from '../pages/Cart';
 import StackRoutes from './StackRoutes';
 
 import { FontAwesome5 as FontIcon } from '@expo/vector-icons';
+import { allProductCart } from '../store/CartProducts/CartProducts.selector';
 
 const Tab = createBottomTabNavigator();
 const Routes:React.FC = () => {
-  
+    const cartProducts = useSelector(allProductCart)
+    
   return(
     <>
      <Tab.Navigator
@@ -32,6 +35,7 @@ const Routes:React.FC = () => {
             tabBarIcon: ({ color, size }) => (
               <FontIcon name="shopping-cart" size={size} color={color} />
             ),
+            tabBarBadge: cartProducts.length
           }}/> 
     </Tab.Navigator>
     </>
