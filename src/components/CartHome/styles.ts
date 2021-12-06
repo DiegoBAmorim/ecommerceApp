4a231758835464dbae7a9792df4fff8c492c8d97
Diego Amorim <66035875+DiegoBAmorim@users.noreply.github.com>
2021-12-06 09:15:36 -0300

    Update styles card

---
+++ src/components/CartHome/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+position: absolute;
+width: 70px;
+height: 70px;
+
+bottom: 41px;
+right: 24px;
+
+border-radius: 100px;
+
+
+align-items: center;
+justify-content: center;
+
+background: #0A0B02;
+
+
+`;  
